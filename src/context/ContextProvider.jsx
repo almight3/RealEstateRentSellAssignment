d238cd6e16a13720d160b50a71749c3ddea24907
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-09-03 02:27:28 +0530

    initialize context for app

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{useReducer} from 'react';
+import data from "../data/RealEstateRent"
+import { rentPropertyData,filterRentProperty } from '../reducer/RentPropertyReducer';
+export const Context = React.createContext(); 
+function ContextProvider({children}) {
+  console.log(data);  
+  const [rentState,rentDispatch] = useReducer(rentPropertyData,{
+   rentProperty:data,
+   favourite:[]
+  })
+  const [filterState,filterDispatch] = useReducer(filterRentProperty,{
+    byLocation:"",
+    byPrice:"",
+    byPropertyType:"",
+    byMoveInDate:"",
+    bySearchQueary:""
+  })
 
-function ContextProvider() {
   return (
-    <div>ContextProvider</div>
+    <Context.Provider>
+      {children}
+    </Context.Provider>
   )
 }
 
