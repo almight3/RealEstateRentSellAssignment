430615ac22e4ac4c9b6e797e8816e43a1abbf979
Priyanshu Shrivastava <shrivastavap136@gmail.com>
2022-09-04 11:58:09 +0530

    feature - added pagination in favourite page

--- src/pages/FavouriteProperty.jsx
+++ src/pages/FavouriteProperty.jsx
@@ -1,18 +1,34 @@
-import React,{useContext} from 'react'
+import React,{useContext,useState} from 'react'
 import {Context} from "../context/ContextProvider";
 import SingleCard from  "../component/SingleCard/SingleCard"
+import Pagination from '../component/common/Pagination';
 import "./page.css"
 function FavouriteProperty() {
   const {rentState} = useContext(Context);
   const {favourite} = rentState;
+  const [page,setPage] = useState(0);
+  
+  let favouriteData = favourite;
+  const pageCount  = Math.ceil(favouriteData && (favouriteData.length / 6));
+  const handelChange = ({selected}) =>{
+    setPage(selected)
+    console.log("rerender",selected)
+  }
+  const propertyPerPage = 6;
+  const pageVisited = page  * propertyPerPage;
+  console.log(favouriteData.slice(pageVisited,pageVisited + propertyPerPage))
+  favouriteData = favouriteData.slice(pageVisited,pageVisited + propertyPerPage)
+
+
   return (
     <div>
       <h4>Totals Items({favourite.length})</h4>
       <div className="property-container">   
         {
-          favourite.map((fav)=><SingleCard property={fav}/>)
+          favouriteData.map((fav)=><SingleCard property={fav}/>)
         }  
     </div>
+    <Pagination pageCount={pageCount} pageChange={handelChange} />
     </div>
   )
 }
